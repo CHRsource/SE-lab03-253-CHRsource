447691dff013781acb744760596f0dd90a2e54ad
Kristina Ivanova <iko_1864@icloud.com>
2024-03-28 22:30:19 +0300

    Лаб 3. Этап 1

---
+++ file-server_Client/Program.cs
@@ -0,0 +1,119 @@
+﻿using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System;
+using System.Threading.Tasks;
+
+class Client
+{
+    public static async Task Main(string[] args)
+    {
+        var port = 12345;
+
+        try
+        {
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+
+            using Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            await socket.ConnectAsync("127.0.0.1", port);
+            Console.WriteLine($"Connection to server {socket.RemoteEndPoint} established.");
+
+            while (true)
+            {
+                Console.Write("Enter action (1 - get a file, 2 - create a file, 3 - delete a file): > ");
+
+                string action = Console.ReadLine();
+
+                //отправка запроса на сервер
+                if (action.Trim().ToLower() == "exit")
+                {
+                    byte[] requestBytesWithData = Encoding.UTF8.GetBytes("exit");
+                    await socket.SendAsync(requestBytesWithData, SocketFlags.None);
+                    Console.WriteLine("The request was sent.");
+                }
+                else if (action == "2")
+                {
+                    //ввод имени файла и его содержимого
+                    Console.Write("Enter filename: > ");
+                    string filename = Console.ReadLine();
+
+                    Console.Write("Enter file content (to complete your entry, press Enter on an empty line):\n> ");
+                    string fileContent = Console.ReadLine(); // Переменная для хранения содержимого файла
+                    string line;
+                    while (true) // Проверяем, что введена не пустая строка
+                    {
+                        Console.Write("> ");
+                        if (string.IsNullOrWhiteSpace(line = Console.ReadLine())) break;
+                        fileContent += "\\n" + line; // Добавляем строку к содержимому файла с разделителем новой строки
+                    }
+                    //отправка имени и содержимого файла на сервер
+                    string request = $"PUT {filename} {fileContent}";
+                    byte[] requestBytesWithData = Encoding.UTF8.GetBytes(request);
+                    await socket.SendAsync(requestBytesWithData, SocketFlags.None);
+                    Console.WriteLine("The request was sent.");
+                }
+                else if (action == "1" || action == "3")
+                {
+                    //ввод имени файла
+                    Console.Write("Enter filename: > ");
+                    string filename = Console.ReadLine();
+
+                    //отправка имени файла на сервер
+                    string request = $"{(action == "1" ? "GET" : "DELETE")} {filename}";
+                    byte[] filenameBytes = Encoding.UTF8.GetBytes(request);
+                    await socket.SendAsync(filenameBytes, SocketFlags.None);
+                    Console.WriteLine("The request was sent.");
+                }
+
+                if (action == "1" || action == "2" || action == "3")
+                {
+                    // Получение ответа от сервера
+                    byte[] responseBytes = new byte[1024];
+                    int bytesRead = await socket.ReceiveAsync(responseBytes, SocketFlags.None);
+                    string response = Encoding.UTF8.GetString(responseBytes, 0, bytesRead);
+                    switch (action)
+                    {
+                        case "1":
+                            {
+                                if (response == "404")
+                                    Console.WriteLine("The response says that the file was not found!");
+                                else
+                                    Console.WriteLine($"The content of the file is:\n{response.Substring(4)}");
+                                break;
+                            };
+                        case "2":
+                            {
+                                if (response == "403")
+                                    Console.WriteLine("The response says that creating the file was forbidden!");
+                                else if (response == "200")
+                                    Console.WriteLine("The response says that the file was created!");
+                                break;
+                            }
+                        case "3":
+                            {
+                                if (response == "404")
+                                    Console.WriteLine("The response says that the file was not found!");
+                                else if (response == "200")
+                                    Console.WriteLine("The response says that the file was successfully deleted!");
+                                break;
+                            }
+                    }
+                }
+            }
+            
+        }
+        catch (SocketException ex)
+        {
+            Console.WriteLine($"Could not establish a connection to the server.");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error: {e.Message}");
+        }
+        finally
+        {
+            // Закрываем соединение с сервером и завершаем работу
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ file-server_Server/Program.cs
@@ -0,0 +1,132 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server
+{
+    // Создаем список для хранения активных клиентских сокетов
+    static List<Socket> clientSockets = new List<Socket>();
+    static string absoluteDataDir = "";
+    static bool flag = true;
+
+    private static async Task HandleClient(Socket clientSocket)
+    {
+
+        // Добавляем клиентский сокет в список активных сокетов
+        clientSockets.Add(clientSocket);
+
+        Console.WriteLine("Client connected!");
+
+        byte[] buffer = new byte[1024];
+
+        while (true)
+        {
+            int bytesRead = clientSocket.Receive(buffer);
+            string request = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Console.WriteLine($"Request received: {request}");
+
+            string[] requestParts = request.Split();
+            string action = requestParts[0];
+
+            // Проверяем, если клиент отправил команду "exit", то завершаем работу сервера
+            if (action == "exit")
+            {
+                // Закрываем все активные клиентские сокеты
+                foreach (var client in clientSockets)
+                {
+                    client.Shutdown(SocketShutdown.Both);
+                    client.Close();
+                }
+                clientSockets.Clear();
+                flag = false;
+                Console.WriteLine("Server shutdown.");
+                Environment.Exit(0); // Завершение работы приложения
+                return; // Завершаем работу сервера
+            }
+
+            string filename = requestParts[1];
+
+            if (action == "GET")
+            {
+                try
+                {
+                    string filePath = Path.Combine(absoluteDataDir, filename);
+                    string fileContent = File.ReadAllText(filePath);
+
+                    byte[] responseBytes = Encoding.UTF8.GetBytes($"200 {fileContent}");
+                    clientSocket.Send(responseBytes);
+                    Console.WriteLine("File content sent.");
+                }
+                catch (FileNotFoundException)
+                {
+                    byte[] responseBytes = Encoding.UTF8.GetBytes("404");
+                    clientSocket.Send(responseBytes);
+                    Console.WriteLine("File was not found.");
+                }
+            }
+            else if (action == "PUT")
+            {
+                string fileData = string.Join(" ", requestParts.Skip(2)).Replace("\\n", "\n");
+                Console.WriteLine(fileData);
+                try
+                {
+                    string filePath = Path.Combine(absoluteDataDir, filename);
+                    if (!File.Exists(filePath))
+                        File.WriteAllText(filePath, fileData);
+                    else throw new IOException("File already exists.");
+                    byte[] responseBytes = Encoding.UTF8.GetBytes("200");
+                    clientSocket.Send(responseBytes);
+                    Console.WriteLine("File created.");
+                }
+                catch (IOException e)
+                {
+                    byte[] responseBytes = Encoding.UTF8.GetBytes("403");
+                    clientSocket.Send(responseBytes);
+                    Console.WriteLine($"{e.Message} Failed to create a file.");
+                }
+            }
+            else if (action == "DELETE")
+            {
+                try
+                {
+                    string filePath = Path.Combine(absoluteDataDir, filename);
+                    if (File.Exists(filePath))
+                        File.Delete(filePath);
+                    else throw new FileNotFoundException();
+
+                    byte[] responseBytes = Encoding.UTF8.GetBytes("200");
+                    clientSocket.Send(responseBytes);
+                    Console.WriteLine("File deleted.");
+                }
+                catch (FileNotFoundException)
+                {
+                    byte[] responseBytes = Encoding.UTF8.GetBytes("404");
+                    clientSocket.Send(responseBytes);
+                    Console.WriteLine("File was not found.");
+                }
+            }
+        }
+    }
+    public static async Task Main(string[] args)
+    {
+        int port = 12345;
+        string dataDir = "\\server\\data\\";
+        string currentDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+        absoluteDataDir = currentDirectory + dataDir;
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        //привязка сокета к локальному IP адресу и порту
+        serverSocket.Bind(new IPEndPoint(IPAddress.Any, port));
+        //установка максимальной длины очереди ожидающих подключений
+        serverSocket.Listen(10);
+        Console.WriteLine("Server started!");
+
+
+        while (flag)
+        {
+            Socket client = await serverSocket.AcceptAsync();
+            _ = Task.Run(() => HandleClient(client));
+        }
+
+    }
+
+}
